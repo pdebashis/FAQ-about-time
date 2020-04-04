843302fba818be1036efe1f093e31317f7ae8811
pdebashis <pdebashis@outlook.com>
2020-04-04 20:13:10 +0530

    Added Scala

--- time.groovy
+++ time.groovy
@@ -1,3 +1,40 @@
+package first
+import groovy.time.Timecategory
+import java.text.SimpleDateFormat
+
+Class Test {
+  static void main(def args){
+    def d = 
+    def d1 = 
+    d =
+    d = 
+    use(TimeCategory){
+      def e = d + 30.minutes                                //import TimeCategory for advanced dateTime calculations
+    }
+    
+    d =
+    d = d.format("",TimeZone.getTimeZone("IST"))            
+    
+    def year = new Date()[0]                                //get the year
+    def month = new Date()[0]                               //get the month
+    def day = new Date()[0]                                 //get the day
+    def arr = new Date()[0]                                 //0-16 components of time
+    
+    def f = new SimpleDateFormat("dd_MM_yyyy").format(d)    //Format String to Time
+    def g = Date.parse("dd_MM_yyyy","21_12_1993")           //Format Time to String
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
 				//or system.currentTime() for current time
 		//time to epoch
 			//epoch to time
--- time.scala
+++ time.scala
@@ -2,22 +2,22 @@ import java.text.SimpleDateFormat
 import java.time.ZoneId
 import java.util.Date
 
-val time = new Date()								//or system.currentTime() for current time
-val epoch = time.getTime()/1000L					//time to epoch
-time = new Date(1529611151)							//epoch to time
+val time = new Date()								               //or system.currentTime() for current time
+val epoch = time.getTime()/1000L					               //time to epoch
+time = new Date(1529611151)							              //epoch to time
 
-time = new Date(epoch + (1000*10))					//add 10 seconds
-time = new Date(epoch + (1000*60*10))				//add 10 minutes(active support)
+time = new Date(epoch + (1000*10))					                 //add 10 seconds
+time = new Date(epoch + (1000*60*10))				               //add 10 minutes(active support)
 
-										//convert to UTC
-										//get the timezone
-									//convert to local zone
-						//convert to any known timezone
+val d = new Date().toInstant.atZone(ZoneId.of("UTC"))				//convert to UTC
+val e = new SimleDateFormat("Z").format(time)                        //get the timezone
+                                                                    //convert to local zone
+val d = new Date().toInstant.atZone(ZoneId.of("IST"))				//convert to any known timezone
 
-										//get the day
-									//get the month
-									//get the year
-									//slit time into 10 components
+val f = new SimleDateFormat("dd").format(time)                      //get the day
+val g = new SimleDateFormat("mm").format(time)						//get the month
+val h = new SimleDateFormat("yy").format(time)						//get the year
 
-							//Format time to string
-			//Format string to time
+
+val i = new SimleDateFormat("dd_MM_YYYY").format(time)							//Format time to string
+val j = new SimleDateFormat("dd_mm_YYYY").parse("21_12_1993")           //Format string to time
