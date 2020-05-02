bc6cfcfc4f556d2d807818dd39092b09649f8a1c
Debashis Pradhan <pdebashis@outlook.com>
2020-05-02 06:00:42 +0530

    update shell commands

--- time.sh
+++ time.sh
@@ -1,19 +1,19 @@
-			#or Time.now for current time
-		#time to epoch
-		#epoch to time
+date			#or Time.now for current time
+date +%s 		#time to epoch
+date -d @$1588379398	#epoch to time
 
-			#add 10 seconds
-			#add 10 minutes(active support)
+#add 10 seconds
+#add 10 minutes(active support)
 
-			#convert to UTC
-			#get the timezone
-			#convert to local zone
-		#convert to any known timezone
+#convert to UTC
+#get the timezone
+#convert to local zone
+#convert to any known timezone
 
-					#get the day
-						#get the month
-							#get the year
-							#slit time into 10 components
+#get the day
+#get the month
+#get the year
+#slit time into components
 
-					#Format time to string
-		#Format string to time
+#Format time to string
+#Format string to time
